220b95937fb0e6c619c258279690f518db3ced14
jiangming <jiangming@tongfangcloud.com>
2017-08-07 17:36:40 +0800

    first

---
+++ src/Tongfang.IdentityManagement/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Tongfang.IdentityManagement.Models
+{
+    /// <summary>
+    /// 用户<see cref="IdentityUser"/>
+    /// </summary>
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+    }
+
+    /// <summary>
+    /// 角色<see cref="IdentityRole"/>
+    /// </summary>
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+    }
+}
